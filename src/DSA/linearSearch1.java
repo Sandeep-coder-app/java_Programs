c202d51f603d267359f968e0c8de7773ccd7b3bc
sk335 <appdevsandeep@gmail.com>
2023-05-26 21:58:58 +0530

    java dsa

--- src/DSA/linearSearch1.java
+++ src/DSA/linearSearch1.java
@@ -8,7 +8,27 @@ public class linearSearch1 {
         int right = length - 1;
         int position = -1;
 
+        for(left = 0; left <= right;)
+        {
+            if(arr[left] == search_element)
+            {
+                position = left;
+                System.out.println("Element found in array at "+(position + 1)+" position with "+(left + 1)+" Attempt");
+                break;
+            }
 
+            if(arr[right] == search_element)
+            {
+                position = right;
+                System.out.println("Element found in array at "+(position + 1)+" position with "+(length - right)+" Attempt");
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if(position == -1)
+            System.out.println("Not found is array with "+left+" Attempt");
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
