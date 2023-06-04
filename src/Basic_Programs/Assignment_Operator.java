258fb91d490225a150830a1be138fe80b2ea852a
hp <appdevsandeep@gmail.com>
2023-06-04 14:17:18 +0530

    java

---
+++ src/Basic_Programs/Assignment_Operator.java
@@ -0,0 +1,24 @@
+package Basic_Programs;
+
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        num1 += num2;
+
+        System.out.println(num1);
+
+        num1 -= num2;
+
+        System.out.println(num1);
+
+        num1 *= num2;
+        System.out.println(num1);
+
+        num1 /= num2;
+        System.out.println(num1);
+
+    }
+}
---
+++ src/Basic_Programs/unary_operator.java
@@ -0,0 +1,13 @@
+package Basic_Programs;
+
+public class unary_operator {
+    public static void main(String[] args) {
+        int a = 10;
+
+//        int b = a++;
+//        System.out.println(b);
+        a++;
+        int b = --a;
+        System.out.println(b);
+    }
+}
